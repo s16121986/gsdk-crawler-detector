4c39302eb997316fb0a70fa8fa02bd168527bb7e
s16121986 <s16121986@yandex.ru>
2023-01-06 15:29:45 +0300

    init

---
+++ CrawlerDetectorMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gsdk\CrawlerDetector;
+
+use Illuminate\Http\Request;
+use Closure;
+
+class CrawlerDetectorMiddleware {
+
+	private static array $bots = [
+		'bot',
+		'slurp',
+		'crawler',
+		'spider',
+		'curl',
+		'facebook',
+		'fetch',
+		'okhttp',
+		'Chrome-Lighthouse'
+	];
+
+	public function handle(Request $request, Closure $next) {
+		$userAgent = $request->header('User-Agent');
+
+		$request->attributes->add(['isCrawlerDetected' => $this->detectBot($userAgent)]);
+
+		return $next($request);
+	}
+
+	private function detectBot($userAgent): bool {
+		foreach (self::$bots as $bot) {
+			if (stripos($userAgent, $bot) !== false)
+				return true;
+		}
+
+		return false;
+	}
+
+}
